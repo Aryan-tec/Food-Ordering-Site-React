f1c612f62aadab9824bb4034b4d296bafb1a836e
Aryan Sanger <aryan.sanger19@gmail.com>
2025-08-02 18:29:43 +0530

    class based component,Life cycle of class bases component,component life cycle

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,26 @@
-const About = () => {
-  return (
+import User from "./User";
+import UserClass from "./UserClass"
+import React from "react";
+
+class About extends React.Component{
+  constructor(porps){
+    super();
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount(){
+    console.log("Paresnt ComponentDidMount")
+  }
+  render(){
+    console.log("Parent Render");
+    return (
     <div>
       <h1>This is About Section</h1>
+      <UserClass  name={"Aryan Sanger (Class)"} location={"Ghaziabad"} />
     </div>
+    
   );
-};
+  }
+}
 
 export default About;
---
+++ src/components/UserClass.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: {
+        name: "dummy",
+        location: "dummy loc",
+      },
+    };
+    console.log("Child Constructor");
+  }
+
+  async componentDidMount() {
+    console.log("Child componentDidMount");
+    const data = await fetch("https://api.github.com/users/Aryan-tec");
+    const json = await data.json();
+    console.log(json);
+    this.setState({
+      userInfo: json,
+    });
+  }
+
+  render() {
+    console.log("Child Render");
+    const { name, location, avatar_url } = this.state.userInfo;
+    return (
+      <div className="user-card">
+        <img src="https://avatars.githubusercontent.com/u/77693306?v=4"></img>
+        <h2>Name : {name}</h2>
+        <h3>Location : {location}</h3>
+        <h4>User Name : @arn_sanger</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
