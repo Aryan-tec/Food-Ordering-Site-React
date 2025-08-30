a01c54fbe63a3b1639e0e9b7a80dd8047871de6d
Aryan Sanger <aryan.sanger19@gmail.com>
2025-08-31 00:34:59 +0530

    Updates

---
+++ Grocery.f2a39477.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire164e;(0,e.register)("loR3J",function(r,a){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>o,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0"),o=()=>(0,i.jsx)("h2",{children:"This is a grocery tab "})});
+//# sourceMappingURL=Grocery.f2a39477.js.map
