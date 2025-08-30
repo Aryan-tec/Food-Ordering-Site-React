c18127247956c2cf6d94558dc9d38de60c88ca14
Aryan Sanger <aryan.sanger19@gmail.com>
2025-08-31 02:23:26 +0530

    feat: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, transformWithEsbuild } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
-base: "/Food-Ordering-Site-React",
-plugins: [react()],
+plugins: [
+{
+name: "treat-js-files-as-jsx",
+async transform(code, id) {
+if (!id.match(/src\/.*\.js$/)) return null;
+return transformWithEsbuild(code, id, {
+loader: "jsx",
+jsx: "automatic",
+});
+},
+},
+react(),
+],
+optimizeDeps: {
+force: true,
+esbuildOptions: {
+loader: { ".js": "jsx" },
+},
+},
+base: "/Food-Ordering-Site-React/",
 });
