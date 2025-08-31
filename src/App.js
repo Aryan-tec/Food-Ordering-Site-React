245318c996e94d50c0d2a03b327e6587327a1f10
Aryan Sanger <aryan.sanger19@gmail.com>
2025-08-31 13:16:25 +0530

    change

--- src/App.js
+++ src/App.js
@@ -39,31 +39,47 @@ const AppLayout = () => {
 
 const Grocery = lazy(() => import("./components/Grocery"));
 
-const appRouter = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <AppLayout />,
-      children: [
-        { path: "/", element: <Body /> },
-        { path: "about", element: <About /> },
-        { path: "contact", element: <Contact /> },
-        {
-          path: "grocery",
-          element: (
-            <Suspense fallback={<h1>Loading....</h1>}>
-              <Grocery />
-            </Suspense>
-          ),
-        },
-        { path: "restaurants/:resId", element: <RestaurantMenu /> },
-        { path: "cart", element: <Cart /> },
-      ],
-      errorElement: <Error />,
-    },
-  ],
-  // { basename: "/Food-Ordering-Site-React" }
-);
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
